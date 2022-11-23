5e9d24cae46af072e7625b563e8fce41525388e4
Abhishek <abhibs19@gmail.com>
2022-11-23 17:48:44 +0530

    new class

---
+++ src/test/java/com/testyantra/branches/New.java
@@ -0,0 +1,14 @@
+package com.testyantra.branches;
+
+import org.testng.annotations.Test;
+
+public class New {
+	
+	
+	@Test
+	public void newClass()
+	{
+		System.out.println("--New class Added---");
+	}
+
+}
