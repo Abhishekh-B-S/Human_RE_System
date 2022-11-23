405307629ee627f4960f5594605b95d704bdbb4d
Abhishek <abhisgowda01@gmail.com>
2022-11-23 15:06:22 +0530

    new package added

---
+++ src/test/java/com/testyantra/employee/Employee.java
@@ -0,0 +1,13 @@
+package com.testyantra.employee;
+
+import org.testng.annotations.Test;
+
+public class Employee {
+
+	
+	@Test
+	public void empDetails()
+	{
+		System.out.println("----Emp details added----");
+	}
+}
