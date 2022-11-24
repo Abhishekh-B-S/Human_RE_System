457785cebca39d3e126123c148514fcb872e2920
Abhishek <abhibs19@gmail.com>
2022-11-24 10:54:48 +0530

    new class

---
+++ src/test/java/com/testyantra/employee/Sample.java
@@ -0,0 +1,13 @@
+package com.testyantra.employee;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	public void sample1()
+	{
+		System.out.println("---New Employee details added with new Branch---");
+	}
+
+}
